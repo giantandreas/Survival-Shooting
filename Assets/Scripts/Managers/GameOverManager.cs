bf1e562f76da81ea043d56c9be806671e603ca9c
Giant Andreas Tambunan <68510068+giantandreas@users.noreply.github.com>
2022-04-07 22:16:31 +0700

    initial commit

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+            Debug.Log("Scene manager = " + SceneManager.GetActiveScene().name);
+            Debug.Log("Restart timer = " + restartTimer);
+            if (restartTimer >= restartDelay)
+            {
+                
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        if (playerHealth.currentHealth > 0)
+        {
+            warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+            anim.SetTrigger("Warning");
+
+        }
+    }
+}
